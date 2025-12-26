ff0f5431046d97147766d2d4e1317b2247fd6d5f
batuhan <batuhan_kanra_60@hotmail.com>
2025-12-26 14:27:45 +0300

    redis connected

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -10,6 +10,11 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	cfg := config.LoadConfig()
 	db.MongoConnect(cfg)
+	redisCLient := db.RedisConnect(cfg.RedisURI, "", 0)
+	if err := redisCLient.Ping(); err != nil {
+		panic(err)
+	}
+	defer redisCLient.Close()
 
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
--- backend/internal/db/redis.go
+++ backend/internal/db/redis.go
@@ -1,1 +1,40 @@
 package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisClient struct {
+	Client *redis.Client
+	ctx    context.Context
+}
+
+func RedisConnect(addr, password string, db int) *RedisClient {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+	return &RedisClient{
+		Client: rdb,
+		ctx:    context.Background(),
+	}
+}
+
+func (rc *RedisClient) Ping() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if _, err := rc.Client.Ping(ctx).Result(); err != nil {
+		return fmt.Errorf("Redis bağlantı hatası: %w", err)
+	}
+	fmt.Println("Ping successful")
+	return nil
+}
+
+func (rc *RedisClient) Close() error {
+	return rc.Client.Close()
+}
